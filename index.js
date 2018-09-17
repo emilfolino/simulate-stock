13bdc76c2fbe6e20594dac49c0368d36fc7b2d77
Emil Folino <emilfolino@gmail.com>
2018-09-17 16:00:53 +0200

    Got it working with dem sockets

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 var stock = require("./stock.js");
 
-console.log(stock.getStockPrice());
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+});
+setInterval(function () {
+    var stocks = {
+        "dbwebb": stock.getStockPrice("dbwebb"),
+        "DIDD": stock.getStockPrice("DIDD"),
+    };
+
+    io.emit("stocks", JSON.stringify(stocks));
+}, 1000);
+
+
+http.listen(8811, function(){
+   console.log('listening on *:8811');
+});
--- stock.js
+++ stock.js
@@ -3,26 +3,31 @@ var stock = {
         return Math.floor(Math.random()*(max-min+1)+min);
     },
 
-    getStockPrice: function () {
+    getStockPrice: function (stockName) {
         const stocks = {
             "dbwebb": {
                 rate: 1.2,
-                startingPoint: 20,
+                startingPoint: 2000,
                 fluctuations: [-8, 10]
+            },
+            "DIDD": {
+                rate: 1.15,
+                startingPoint: 6000,
+                fluctuations: [-2, 8]
             }
         };
 
-        var startT = Math.floor(new Date(2018, 8, 17) / 1000)
+        var startT = Math.floor(new Date(2018, 8, 17, 14) / 1000)
         var t = Math.floor(new Date() / 1000);
-        var rate = stocks["dbwebb"]["rate"];
-        var startingPoint = stocks["dbwebb"]["startingPoint"];
-        var delta = t - startT;
+        var rate = stocks[stockName]["rate"];
+        var startingPoint = stocks[stockName]["startingPoint"];
+        var delta = (t - startT);
         var fluctuation = stock.randomInt(
-            stocks["dbwebb"]["fluctuations"][0],
-            stocks["dbwebb"]["fluctuations"][1]
+            stocks[stockName]["fluctuations"][0],
+            stocks[stockName]["fluctuations"][1]
         );
 
-        return rate * delta + startingPoint + fluctuation;
+        return (rate * delta + startingPoint + fluctuation) / 1000;
     }
 };
 
