2ded71d08e12f8f6b99ff7b678317a021e279ddd
Emil Folino <emilfolino@gmail.com>
2018-11-30 11:32:29 +0100

    Made better structured versions with text and correct series names

--- stockClient.js
+++ stockClient.js
@@ -19,12 +19,48 @@ import 'rickshaw/rickshaw.min.css';
 
     socket.on('stocks', (message) => {
         if (first) {
+            var palette = new Rickshaw.Color.Palette({ scheme: 'colorwheel' });
             message.map((cake, index) => {
-                let graph = createGraph(graphContainer, cake, index);
+                let graphTitle = document.createElement("h1");
+
+                graphTitle.textContent = cake.name;
+
+                let graphElement = document.createElement("div");
+
+                graphContainer.appendChild(graphTitle);
+                graphContainer.appendChild(graphElement);
+
+                let graph = new Rickshaw.Graph({
+                    element: graphElement,
+                    width: "500",
+                    height: "300",
+                    renderer: "line",
+                    series: new Rickshaw.Series.FixedDuration([{
+                        name: cake.name,
+                        color: palette.color(),
+                    }], undefined, {
+                        timeInterval: 5000,
+                        maxDataPoints: 1000,
+                        timeBase: new Date().getTime() / 1000
+                    })
+                });
 
                 graph.configure({
                     width: graphContainer.clientWidth,
                 });
+
+                var axes = new Rickshaw.Graph.Axis.Time( { graph: graph } );
+
+                var y_ticks = new Rickshaw.Graph.Axis.Y({
+                    graph: graph,
+                    orientation: 'left',
+                    tickFormat: Rickshaw.Fixtures.Number.formatKMBT
+                });
+
+                var hoverDetail = new Rickshaw.Graph.HoverDetail({
+                    graph: graph
+                });
+
                 graph.render();
 
                 let slug = slugify(cake.name);
@@ -32,7 +68,6 @@ import 'rickshaw/rickshaw.min.css';
                 graphs[slug] = {
                     name: cake.name,
                     graph: graph,
-                    series: [],
                 };
             });
             first = false;
@@ -40,10 +75,10 @@ import 'rickshaw/rickshaw.min.css';
 
         message.map((cake) => {
             let slug = slugify(cake.name);
+            let data = {};
+            data[cake.name] = cake.startingPoint;
 
-            graphs[slug].graph.series.addData({
-                y: cake.startingPoint,
-            });
+            graphs[slug].graph.series.addData(data);
             graphs[slug].graph.render();
         });
 
@@ -52,32 +87,10 @@ import 'rickshaw/rickshaw.min.css';
 })();
 
 function slugify(text) {
-  return text.toString().toLowerCase()
+    return text.toString().toLowerCase()
     .replace(/\s+/g, '-')           // Replace spaces with -
     .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
     .replace(/\-\-+/g, '-')         // Replace multiple - with single -
     .replace(/^-+/, '')             // Trim - from start of text
     .replace(/-+$/, '');            // Trim - from end of text
-}
-
-function createGraph(parent, element, index) {
-    let colors = ["#ff851b", "#0074d9"];
-    let graphElement = document.createElement("div");
-
-    parent.appendChild(graphElement);
-
-    return new Rickshaw.Graph({
-        element: graphElement,
-        width: "500",
-        height: "180",
-        renderer: "line",
-        series: new Rickshaw.Series.FixedDuration([{
-            name: element.name,
-            color: colors[index%(colors.length)],
-        }], undefined, {
-            timeInterval: 5000,
-            maxDataPoints: 1000,
-            timeBase: new Date().getTime() / 1000
-        })
-    });
 }
